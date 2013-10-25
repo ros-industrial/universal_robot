264385a46063a44a3b24ec6ee653ac4590e2806f
hyatt <hyatt@hyatt.(none)>
2013-10-24 21:32:54 -0400

    Converted joint_vel_ctrl from pos/vel/acc to just velocity control.

--- ur_joint_ctrl_gui/src/ur_joint_ctrl_gui/joint_vel_ctrl.py
+++ ur_joint_ctrl_gui/src/ur_joint_ctrl_gui/joint_vel_ctrl.py
@@ -12,37 +12,39 @@ class JointVelocityController(object):
         self.vel_f = 0.2
         self.t_f = 0.5 
         self.acc = self.vel_f / self.t_f
-        self.pos_f = 0.5*self.acc*(self.t_f**2)
+        # self.pos_f = 0.5*self.acc*(self.t_f**2)
         self.t = 0.0
         js_prefix = rospy.get_param("~js_prefix", "")
         self.arm_iface = ArmInterface(js_prefix=js_prefix)
-        self.q_init = None
+        # self.q_init = None
 
     def update(self):
         if self.moving_joint is not None:
-            t, t_f, vel_f, acc, pos_f = self.t, self.t_f, self.vel_f, self.acc, self.pos_f
-            q_cmd = self.q_init.copy()
+            t, t_f, vel_f, acc = self.t, self.t_f, self.vel_f, self.acc
+            # t, t_f, vel_f, acc, pos_f = self.t, self.t_f, self.vel_f, self.acc, self.pos_f
+            # q_cmd = self.q_init.copy()
             qd_cmd = np.zeros(6)
-            qdd_cmd = np.zeros(6)
+            # qdd_cmd = np.zeros(6)
             if t < t_f:
-                q_cmd[self.moving_joint] += self.moving_dir*(0.5*acc*(t**2))
+                # q_cmd[self.moving_joint] += self.moving_dir*(0.5*acc*(t**2))
                 qd_cmd[self.moving_joint] += self.moving_dir*(acc*t)
-                qdd_cmd[self.moving_joint] += self.moving_dir*(acc)
+                # qdd_cmd[self.moving_joint] += self.moving_dir*(acc)
             else:
-                q_cmd[self.moving_joint] += self.moving_dir*(vel_f*(t-t_f) + pos_f)
+                # q_cmd[self.moving_joint] += self.moving_dir*(vel_f*(t-t_f) + pos_f)
                 qd_cmd[self.moving_joint] = self.moving_dir*vel_f
-            self.arm_iface.cmd_pos_vel_acc(q_cmd, qd_cmd, qdd_cmd)
+            # self.arm_iface.cmd_pos_vel_acc(q_cmd, qd_cmd, qdd_cmd)
+            self.arm_iface.cmd_vel(qd_cmd)
             self.t += 1.0/self.arm_iface.CONTROL_RATE
 
     def start_moving(self, joint_ind, direction, vel):
-        self.arm_iface.unlock_security_stop()
+        # self.arm_iface.unlock_security_stop()
         self.moving_joint = joint_ind
         self.moving_dir = direction
         self.t = 0.0
         self.vel_f = vel
         self.acc = self.vel_f / self.t_f
-        self.pos_f = 0.5*self.acc*(self.t_f**2)
-        self.q_init = np.array(self.arm_iface.get_q())
+        # self.pos_f = 0.5*self.acc*(self.t_f**2)
+        # self.q_init = np.array(self.arm_iface.get_q())
 
     def stop_moving(self):
         self.moving_joint = None
