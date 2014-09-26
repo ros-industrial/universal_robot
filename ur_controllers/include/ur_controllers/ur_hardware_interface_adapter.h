0a25d95a03739f30f0a20b057f8f44a4456dce33
shadowmanos <manos@shadowrobot.com>
2014-09-26 11:19:19 +0100

    modify transmissions to be compatible with ros_ethercat. modify interface to agree with the latest indigo-devel branch of ros_control. modify CMakeLists to resolve build errors.

--- ur_controllers/include/ur_controllers/ur_hardware_interface_adapter.h
+++ ur_controllers/include/ur_controllers/ur_hardware_interface_adapter.h
@@ -3,39 +3,39 @@
 #include <ur_ctrl_client/pos_vel_acc_joint_iface.h>
 #include <trajectory_interface/pos_vel_acc_state.h>
 
-/**
- * \brief Adapter for a velocity-controlled hardware interface. Forwards desired velocities as commands.
- */
-template <class State>
-class HardwareInterfaceAdapter<hardware_interface::VelocityJointInterface, State>
-{
-public:
-  HardwareInterfaceAdapter() : joint_handles_ptr_(0) {}
-
-  bool init(std::vector<hardware_interface::JointHandle>& joint_handles, ros::NodeHandle& controller_nh)
-  {
-    // Store pointer to joint handles
-    joint_handles_ptr_ = &joint_handles;
-
-    return true;
-  }
-
-  void starting(const ros::Time& time) {}
-  void stopping(const ros::Time& time) {}
-
-  void updateCommand(const ros::Time&     /*time*/,
-                     const ros::Duration& /*period*/,
-                     const State&         desired_state,
-                     const State&         /*state_error*/)
-  {
-    // Forward desired position to command
-    const unsigned int n_joints = joint_handles_ptr_->size();
-    for (unsigned int i = 0; i < n_joints; ++i) {(*joint_handles_ptr_)[i].setCommand(desired_state.velocity[i]);}
-  }
-
-private:
-  std::vector<hardware_interface::JointHandle>* joint_handles_ptr_;
-};
+///**
+// * \brief Adapter for a velocity-controlled hardware interface. Forwards desired velocities as commands.
+// */
+//template <class State>
+//class HardwareInterfaceAdapter<hardware_interface::VelocityJointInterface, State>
+//{
+//public:
+//  HardwareInterfaceAdapter() : joint_handles_ptr_(0) {}
+//
+//  bool init(std::vector<hardware_interface::JointHandle>& joint_handles, ros::NodeHandle& controller_nh)
+//  {
+//    // Store pointer to joint handles
+//    joint_handles_ptr_ = &joint_handles;
+//
+//    return true;
+//  }
+//
+//  void starting(const ros::Time& time) {}
+//  void stopping(const ros::Time& time) {}
+//
+//  void updateCommand(const ros::Time&     /*time*/,
+//                     const ros::Duration& /*period*/,
+//                     const State&         desired_state,
+//                     const State&         /*state_error*/)
+//  {
+//    // Forward desired position to command
+//    const unsigned int n_joints = joint_handles_ptr_->size();
+//    for (unsigned int i = 0; i < n_joints; ++i) {(*joint_handles_ptr_)[i].setCommand(desired_state.velocity[i]);}
+//  }
+//
+//private:
+//  std::vector<hardware_interface::JointHandle>* joint_handles_ptr_;
+//};
 
 // /**
 //  * \brief Adapter for a position/velocity/acceleration-controlled hardware interface. 
